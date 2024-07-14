210b7dcfdcc686d7604f70f3b402278179e6a549
Harsh Pandey <harshp6421@gmail.com>
2024-07-14 23:04:01 +0530

    Day 1 done

---
+++ Day1.js
@@ -0,0 +1,29 @@
+// Activity 1
+var i = 10;
+console.log(i);
+
+let j = "Hey";
+console.log(j);
+
+// Activity 2
+const bool = true;
+console.log(bool);
+
+// Activity 3
+let a = 10;
+let b = "Ohh";
+let c = true;
+let obj = { 1: "Value", 2: "Another" };
+let arr = [1, "2", true];
+
+console.table([a, b, c, i, obj, arr]);
+
+// Activity 4
+let m = 10;
+console.log(m);
+m = 20;
+console.log(m);
+
+// Activity 5
+bool = false; //Error assignment to constant variable
+console.log(bool);
